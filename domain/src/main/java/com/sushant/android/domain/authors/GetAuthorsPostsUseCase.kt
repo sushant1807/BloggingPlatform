b7b1d134be29b03cd04e712e02265207b7f26fc4
Sushant <sushant.attada@epita.fr>
2020-09-28 15:08:52 +0200

    Get posts by author id from activity
    Get posts by author id from activity

--- domain/src/main/java/com/sushant/android/domain/authors/GetAuthorsPostsUseCase.kt
+++ domain/src/main/java/com/sushant/android/domain/authors/GetAuthorsPostsUseCase.kt
@@ -1,4 +1,15 @@
 package com.sushant.android.domain.authors
 
-class GetAuthorsPostsUseCase {
+import com.sushant.android.data.authors.model.Post
+import com.sushant.android.data.repository.AuthorsRepository
+import com.sushant.android.domain.UseCasePosts
+import io.reactivex.Scheduler
+import io.reactivex.Single
+import javax.inject.Inject
+
+class GetAuthorsPostsUseCase @Inject constructor(private val authorsRepository: AuthorsRepository,
+                                                 subscribeScheduler: Scheduler,
+                                                 postExecutionScheduler: Scheduler
+) : UseCasePosts<List<Post>, Unit>(subscribeScheduler, postExecutionScheduler) {
+    override fun buildUseCaseSingle(params: Unit?): Single<List<Post>> = authorsRepository.postsByAuthorID(12)
 }
