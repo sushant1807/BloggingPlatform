a467aa3cfa25904643ca9138e2a89b5510d0c5cd
Sushant <sushant.attada@epita.fr>
2020-10-01 14:49:52 +0200

    UI changes to update the user information
    UI changes to update the user information

--- app/src/main/java/com/sushant/android/bloggingplatform/presentation/ui/author_detail/AuthorDetailActivity.kt
+++ app/src/main/java/com/sushant/android/bloggingplatform/presentation/ui/author_detail/AuthorDetailActivity.kt
@@ -1,6 +1,7 @@
 package com.sushant.android.bloggingplatform.presentation.ui.author_detail
 
 import android.content.Intent
+import com.squareup.picasso.Picasso
 import com.sushant.android.bloggingplatform.App
 import com.sushant.android.bloggingplatform.R
 import com.sushant.android.bloggingplatform.presentation.component.DaggerAuthorDetailComponent
@@ -8,13 +9,14 @@ import com.sushant.android.bloggingplatform.presentation.ui.PostDetailActivity
 import com.sushant.android.data.authors.model.Author
 import com.sushant.android.data.authors.model.Post
 import com.sushant.android.mvp.CleanActivity
-import kotlinx.android.synthetic.main.activity_authors.*
+import kotlinx.android.synthetic.main.activity_authors.authors_list_recycler_view
+import kotlinx.android.synthetic.main.activity_posts.*
 
 class AuthorDetailActivity : CleanActivity<AuthorsDetailPresenter>(), AuthorsDetailView, OnItemClickListner {
 
     lateinit var receivedItem: Author
 
-    override fun getLayout(): Int = R.layout.activity_authors
+    override fun getLayout(): Int = R.layout.activity_posts
 
     override fun initInjector() {
         receivedItem = (intent.getSerializableExtra("AUTHOR") as? Author)!!
@@ -34,6 +36,17 @@ class AuthorDetailActivity : CleanActivity<AuthorsDetailPresenter>(), AuthorsDet
 
     override fun showPostList(authors: List<Post>) {
         authors_list_recycler_view.adapter = AuthorsListAdapter(this, authors, this)
+
+        user_name.setText(receivedItem.name)
+        email_text.setText(receivedItem.email)
+        Picasso.get()
+            .load(receivedItem.avatarUrl)
+            .apply {
+                fit()
+                centerCrop()
+                placeholder(R.drawable.ic_launcher_foreground)
+                into(image_user)
+            }
     }
 
     override fun onItemClick(item: Post, position: Int) {
@@ -42,5 +55,4 @@ class AuthorDetailActivity : CleanActivity<AuthorsDetailPresenter>(), AuthorsDet
         intent.putExtra("AUTHOR", receivedItem)
         startActivity(intent)
     }
-
 }
--- app/src/main/java/com/sushant/android/bloggingplatform/presentation/ui/author_list/AuthorsListAdapter.kt
+++ app/src/main/java/com/sushant/android/bloggingplatform/presentation/ui/author_list/AuthorsListAdapter.kt
@@ -8,69 +8,49 @@ import com.squareup.picasso.Picasso
 import com.sushant.android.bloggingplatform.R
 import com.sushant.android.data.authors.model.Author
 import kotlinx.android.synthetic.main.list_item_author.view.*
-import java.text.DateFormat
-import java.text.ParseException
-import java.text.SimpleDateFormat
-import java.util.*
-
 
 class AuthorsListAdapter(
   private val context: Context,
   private val authors: List<Author>,
   var clickListner: OnItemClickListner
 ) :
-  androidx.recyclerview.widget.RecyclerView.Adapter<AuthorsListAdapter.ViewHolder>() {
+    androidx.recyclerview.widget.RecyclerView.Adapter<AuthorsListAdapter.ViewHolder>() {
 
-  class ViewHolder(val view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view)
+    class ViewHolder(val view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view)
 
-  override fun onCreateViewHolder(
-    parent: ViewGroup,
-    viewType: Int
-  ): ViewHolder {
-    val relativeLayout = LayoutInflater.from(context)
-      .inflate(R.layout.list_item_author, parent, false)
-    return ViewHolder(relativeLayout)
-  }
+    override fun onCreateViewHolder(
+      parent: ViewGroup,
+      viewType: Int
+    ): ViewHolder {
+        val relativeLayout = LayoutInflater.from(context)
+            .inflate(R.layout.list_item_author, parent, false)
+        return ViewHolder(relativeLayout)
+    }
 
-  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-    val datewithoutTZ: DateFormat = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
-    holder.view.user_name_text.setText(authors[position].name)
-    holder.view.email_text.setText(authors[position].email)
+        holder.view.user_name_text.setText(authors[position].name)
+        holder.view.email_text.setText(authors[position].email)
 
-    Picasso.get()
+        Picasso.get()
             .load(authors[position].avatarUrl)
             .apply {
-              fit()
-              centerCrop()
-              placeholder(R.drawable.ic_launcher_foreground)
-              into(holder.view.image_avatar)
+                fit()
+                centerCrop()
+                placeholder(R.drawable.ic_launcher_foreground)
+                into(holder.view.image_avatar)
             }
 
-    holder.view.setOnClickListener {
-      clickListner.onItemClick(authors[position], position)
+        holder.view.setOnClickListener {
+            clickListner.onItemClick(authors[position], position)
 
-    }
-  }
-
-  fun convertISOTimeToDate(isoTime: String): String? {
-    val sdf = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", Locale.ENGLISH)
-    var convertedDate: Date? = null
-    var formattedDate: String? = null
-    try {
-      convertedDate = sdf.parse(isoTime)
-      formattedDate = SimpleDateFormat("dd-MM-yyyy" + "\n" + " hh:mm:ss a", Locale.ENGLISH).format(convertedDate)
-    } catch (e: ParseException) {
-      e.printStackTrace()
+        }
     }
 
-    return formattedDate
-  }
-
-  override fun getItemCount() = authors.size
+    override fun getItemCount() = authors.size
 
 }
 
-interface OnItemClickListner{
-  fun onItemClick(item: Author, position: Int)
+interface OnItemClickListner {
+    fun onItemClick(item: Author, position: Int)
 }
